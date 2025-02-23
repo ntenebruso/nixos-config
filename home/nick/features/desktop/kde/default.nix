ac07eff003cbbb462a0b9e8bf34c1e737615edfb
Nick Tenebruso <nickten2006@gmail.com>
2025-02-22 20:00:08 -0500

    Start with empty session

--- home/nick/features/desktop/kde/default.nix
+++ home/nick/features/desktop/kde/default.nix
@@ -28,6 +28,9 @@
     kscreenlocker.appearance = {
       wallpaper = ./../../wallpapers/pictures/sunset.png;
     };
+    session = {
+      sessionRestore.restoreOpenApplicationsOnLogin = "startWithEmptySession";
+    };
   };
 
   programs.konsole = {
